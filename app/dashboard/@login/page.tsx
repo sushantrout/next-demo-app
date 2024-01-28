6bb90d105da344c2144061f758e070beeedf0e26
Sushant Kumar Rout <38247082+sushantrout@users.noreply.github.com>
2024-01-28 21:27:09 +0530

    Conditional routing

---
+++ app/dashboard/@login/page.tsx
@@ -0,0 +1,3 @@
+export default function LoginConditionalPage() {
+    return <div>Login Page</div>
+}
--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -3,12 +3,15 @@ export default function Dashboard({
     dashboard1,
     dashboard2,
     dashboard3,
+    login
   }: Readonly<{
     children: React.ReactNode;
     dashboard1: React.ReactNode;
     dashboard2: React.ReactNode;
     dashboard3: React.ReactNode;
+    login : React.ReactNode;
   }>) {
+    let isLoggedIn = false;
     return (
         <div>
         <h1>Dashboard Layout</h1>
@@ -17,6 +20,7 @@ export default function Dashboard({
         {dashboard1}
         {dashboard2}
         {dashboard3}
+        {isLoggedIn ? login : <>TESTS</>}
         </div>
     );
 }
